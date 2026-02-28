c249790632396616b90ee111adea2ac07baf6585
sienaindigoandlavender <siena@indigoandlavender.love>
2026-02-28 22:17:12 +0000

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
     ],
   },
 };
